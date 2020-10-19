91e006f0d9b0563e293e81d4479152f34fe4df7b
jasonrobwebster <jasonrobwebster@gmail.com>
2020-10-19 22:07:01 +0200

    refactored player code to hide private functions

--- player/Player.gd
+++ player/Player.gd
@@ -11,7 +11,7 @@ enum States {
 const MAX_FALL_SPEED := 400
 const MAX_MOVE_SPEED := 64
 const MAX_DECCELERATION := 50
-const JUMP_FORCE := 190
+const JUMP_FORCE := 170
 const FLOOR_ACC := 25
 const AIR_ACC := 1
 const FLOOR_FRICTION := 25
@@ -21,7 +21,8 @@ const HOVER_SPEED := 30
 
 var velocity := Vector2.ZERO
 var state
-var snap := true
+
+var _snap := true
 var friction := 0
 var acceleration := 0
 
@@ -33,22 +34,29 @@ onready var coyote_timer = $CoyoteTimer
 
 
 func _ready():
-	state_change(States.MOVE)
+	_state_change(States.MOVE)
 
 
 func _physics_process(delta):
 	var fdelta: float = delta * global.TARGET_FPS
-	
+	_update_fsm(fdelta)
+	_apply_gravity(fdelta)
+	_caluculate_velocity(fdelta)
+	_move()
+
+
+func _update_fsm(fdelta):
+	match state:
+		States.IDLE, States.MOVE:
+			_state_move(fdelta)
+		States.JUMP:
+			_state_jump(fdelta)
+
+
+func _caluculate_velocity(fdelta):
 	var input_right := Input.get_action_strength("ui_right")
 	var input_left := Input.get_action_strength("ui_left")
 	var input_x := input_right - input_left
-	
-	match state:
-		States.IDLE, States.MOVE:
-			state_move(fdelta)
-		States.JUMP:
-			state_jump(fdelta)
-	
 	if input_x != 0:
 		velocity.x = move_toward(
 			velocity.x, 
@@ -56,7 +64,7 @@ func _physics_process(delta):
 			acceleration * fdelta
 		)
 		if state != States.JUMP:
-			state_change(States.MOVE)
+			_state_change(States.MOVE)
 		sprite.flip_h = input_x > 0
 		sprite.offset.x = - int(input_x > 0)
 	else:
@@ -66,42 +74,36 @@ func _physics_process(delta):
 			friction * fdelta
 		)
 		if state != States.JUMP:
-			state_change(States.IDLE)
-	
-	bring_speed_to_max(fdelta)
-	move()
+			_state_change(States.IDLE)
+	_bring_speed_to_max(fdelta)
 
 
-func state_move(fdelta):
+func _state_move(fdelta):
 	if not is_on_floor():
-		state_change(States.JUMP)
+		_state_change(States.JUMP)
 		coyote_timer.start(COYOTE_TIME)
 	if Input.is_action_just_pressed("jump"):
 		velocity += global.up_direction * JUMP_FORCE
-		state_change(States.JUMP)
-	apply_gravity(fdelta)
+		_state_change(States.JUMP)
 
 
-func state_jump(fdelta):
+func _state_jump(fdelta):
 	var jump_speed := velocity.dot(global.up_direction)
-	
 	if jump_speed > HOVER_SPEED and anim_player.current_animation != "JumpDown":
 		anim_player.play("JumpDown")
 	elif -HOVER_SPEED <= jump_speed and jump_speed <= HOVER_SPEED and anim_player.current_animation != "JumpMid":
 		anim_player.play("JumpMid")
 	elif jump_speed < -HOVER_SPEED and anim_player.current_animation != "JumpUp":
 		anim_player.play("JumpUp")
-		
 	if Input.is_action_just_pressed("jump") and coyote_timer.time_left > 0:
 		velocity.y = 0
 		velocity += global.up_direction * JUMP_FORCE
 		coyote_timer.stop()
 	if is_on_floor():
-		state_change(States.MOVE)
-	apply_gravity(fdelta)
+		_state_change(States.MOVE)
 
 
-func state_change(desired_state):
+func _state_change(desired_state):
 	if state == desired_state:
 		return
 	match desired_state:
@@ -113,20 +115,20 @@ func state_change(desired_state):
 			anim_player.playback_speed = 1
 			friction = FLOOR_FRICTION
 			acceleration = FLOOR_ACC
-			snap = true
+			_snap = true
 		States.JUMP:
 			anim_player.play("JumpUp")
 			friction = AIR_FRICTION
 			acceleration = AIR_ACC
-			snap = false
+			_snap = false
 	state = desired_state
 
 
-func apply_gravity(fdelta):
+func _apply_gravity(fdelta):
 	velocity -= global.gravity * global.up_direction * fdelta
 
 
-func bring_speed_to_max(fdelta):
+func _bring_speed_to_max(fdelta):
 	if abs(velocity.x) > MAX_MOVE_SPEED:
 		velocity.x = move_toward(
 			velocity.x,
@@ -141,9 +143,9 @@ func bring_speed_to_max(fdelta):
 		)
 
 
-func move():
+func _move():
 	velocity = move_and_slide_with_snap(
 		velocity, 
-		-global.up_direction * float(snap), 
+		-global.up_direction * float(_snap), 
 		global.up_direction
 	)
