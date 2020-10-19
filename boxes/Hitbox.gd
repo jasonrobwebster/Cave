cddc47bfc8021b0d157a99639a0255e54b7cc6b9
jasonrobwebster <jasonrobwebster@gmail.com>
2020-10-19 22:06:14 +0200

    added hitboxes and hurtboxes

---
+++ boxes/Hitbox.gd
@@ -0,0 +1,4 @@
+class_name Hitbox
+extends Area2D
+
+export(int) var damage = 1
---
+++ boxes/Hurtbox.gd
@@ -0,0 +1,36 @@
+class_name Hurtbox
+extends Area2D
+
+export(float, 0, 2) var invincible_time = 0.5
+
+var invincible := false setget set_invincibility
+
+signal invincibility_started
+signal invincibility_ended
+
+
+func set_invincibility(value: bool):
+	invincible = value
+	if value:
+		emit_signal("invincibility_started")
+	else:
+		emit_signal("invincibility_ended")
+
+
+func _on_Timer_timeout():
+	self.invincible = false
+
+
+func _on_Hurtbox_area_entered(area: Hitbox):
+	owner.health -= area.damage
+	self.invincible = true
+
+
+func _on_Hurtbox_invincibility_started():
+	$Timer.start(invincible_time)
+	if invincible_time > 0:
+		set_deferred("monitoring", false)
+
+
+func _on_Hurtbox_invincibility_ended():
+	monitoring = true
