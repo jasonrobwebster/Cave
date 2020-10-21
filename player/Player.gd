442075eb704b9fd12daaddcb9f3dd9be18e522be
Jason Webster <jasonrobwebster@gmail.com>
2020-10-21 19:26:01 +0200

    Added hurtbox, hitbox, and health to player

--- player/Player.gd
+++ player/Player.gd
@@ -1,2 +1,24 @@
 extends KinematicBody2D
 
+signal max_health_changed(new_value)
+signal health_changed(new_value)
+
+export(int) var max_health setget set_maxhealth
+
+onready var health: int = max_health setget set_health
+
+
+func _on_Hurtbox_area_entered(area: Hitbox):
+	if area.get("damage"):
+		self.health -= area.damage
+
+
+func set_health(value):
+	health = clamp(value, 0, max_health)
+	emit_signal("health_changed", health)
+
+
+func set_maxhealth(value):
+	max_health = max(0, value)
+	self.health = health
+	emit_signal("max_health_changed", max_health)
