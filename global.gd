b2c35cc280fa6a9f74582df94262854078e44622
jasonrobwebster <jasonrobwebster@gmail.com>
2020-11-22 17:28:19 +0200

    Update global.gd

--- global.gd
+++ global.gd
@@ -1,7 +1,7 @@
 extends Node
 
-const TARGET_FPS = 60
-const PIXELS_PER_METER = 32
+const TARGET_FPS: int = 60
+const PIXELS_PER_METER: int = 32
 
 var gravity = 25.0 * PIXELS_PER_METER / TARGET_FPS
 var up_direction = Vector2.UP
