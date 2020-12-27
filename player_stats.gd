d17edd93da3b34da916e68db4da03e98161336fe
jasonrobwebster <jasonrobwebster@gmail.com>
2020-12-27 20:09:56 +0200

    added equip texture and changed max health function

--- player_stats.gd
+++ player_stats.gd
@@ -2,9 +2,11 @@ extends Node
 
 signal max_health_changed(new_value)
 signal health_changed(new_value)
+signal equip_changed(new_texture)
 
 export(int) var max_health = 5 setget set_maxhealth
 export(int) var health = 5 setget set_health
+export(Texture) var equip_texture = null setget set_equip
 
 
 func set_health(value: int):
@@ -13,7 +15,13 @@ func set_health(value: int):
 
 
 func set_maxhealth(value: int):
+	var diff: int = value - max_health
 	max_health = max(1, value)
 	if health:
-		self.health = health
+		self.health = health + diff if diff > 0 else health
 	emit_signal("max_health_changed", max_health)
+
+
+func set_equip(value: Texture):
+	equip_texture = value
+	emit_signal("equip_changed", value)
