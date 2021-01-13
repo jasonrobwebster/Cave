946837f5ce698d19a8a0977266c5ad3a4440a521
jasonrobwebster <jasonrobwebster@gmail.com>
2021-01-13 22:05:35 +0200

    adjust treasure behaviour and add doorway spawning

--- scenes/Level1/ObjectSpawner.gd
+++ scenes/Level1/ObjectSpawner.gd
@@ -4,11 +4,15 @@ extends Node2D
 export(float, 0, 1) var path_room_spawn_multiplier = 1
 export(float, 0, 1) var treasure_room_spawn_multiplier = 1
 export(float, 0, 1) var enemy_spawn_mult = 1
+export(float, 0, 10, .1) var treasure_spawn_mult = 1
+export(bool) var treasures_on_ground = false
 export(Array, PackedScene) var ground_enemies = []
 export(Array, PackedScene) var rooftop_enemies = []
 export(Array, PackedScene) var wall_enemies = []
 export(Array, PackedScene) var flying_enemies = []
 export(Array, PackedScene) var treasures = []
+export(Array, String, FILE, "*.tscn,*scn") var next_levels = [null]
+export(Array, String, FILE, "*.tscn,*scn") var next_rooms = [null]
 
 enum Placement {
 	FLOOR, WALL, ROOF, AIR
@@ -22,6 +26,7 @@ var _player_room: Vector2
 var _room_size: Vector2
 var _tile_size: Vector2
 var _rng = RandomNumberGenerator.new()
+var _doorway: PackedScene = preload("res://interactable/doorways/Level1/Doorway.tscn")
 
 
 func _ready():
@@ -39,6 +44,7 @@ func _place_enemies():
 	_spawn_enemies(wall_enemies, wall_points)
 	_spawn_enemies(flying_enemies, air_points)
 	_place_treasures(treasures, ground_points, empty_points)
+	_place_doorway(empty_points)
 
 
 func _spawn_enemies(
@@ -87,13 +93,69 @@ func _place_treasures(treasures: Array, points: Array, empty_points: Array):
 		if not spawn_chance:
 			spawn_chance = SPAWN_CHANCE_DEFAULT
 		var surrounding_empty := _count_surrounding_empty_points(point, empty_points)
-		var surrounding_fill := 4 - surrounding_empty - 1
-		var proportion_fill := surrounding_fill / 3
-		if _rng.randf() > spawn_chance * spawn_mult * proportion_fill:
+		var surrounding_fill := 4 - surrounding_empty - (1 - int(treasures_on_ground))
+		var proportion_fill := surrounding_fill / (3 + int(treasures_on_ground))
+		if _rng.randf() > spawn_chance * spawn_mult * proportion_fill * treasure_spawn_mult:
 			treasure.queue_free()
 			continue
 		treasure.global_position = point + _tile_size.project(Vector2.UP) / 2
 		add_child(treasure)
+
+
+func _place_doorway(empty_points: Array):
+	# find empty cells in the end room
+	var empty_doorway_points := []
+	var space_state = get_world_2d().get_direct_space_state()
+	var room_tilevs := _get_room_tilevs()
+	
+	var i: int = 1
+	var valid_points: Array = []
+	while i < len(_path):
+		var room_offset = _roomv_to_worldv(_path[-i])
+		for tilev in room_tilevs:
+			var tile_offset = _tilev_to_worldv(tilev)
+			var worldv = room_offset + tile_offset + _tile_size/2
+			var collision_check = space_state.intersect_point(worldv, 32, [], 1, true)
+			if collision_check:
+				continue
+			empty_doorway_points.push_back(worldv)
+		
+		# find the empty points on the ground
+		var ground_doorway_points: Array = _find_empty_ground_worldvs(empty_doorway_points)
+		
+		# We limit the placement of a doorway to any point that has empty points
+		# to the left, right, and above the door. This is to prevent the door from
+		# being entombed while also giving some breathing room from the walls
+		# also ensure there is a ground point to the left and right of the
+		# door, so the door doesn't spawn on a cliff
+		for point in ground_doorway_points:
+			var surrounding_empty := _count_surrounding_empty_points(point, empty_points)
+			if surrounding_empty != 3:
+				continue
+			if not (
+				(point + _tile_size.project(Vector2.RIGHT) in ground_doorway_points)
+				and (point - _tile_size.project(Vector2.RIGHT) in ground_doorway_points)
+			):
+				continue
+			
+			valid_points.push_back(point)
+		
+		# if there is at least one valid point for our door, we exit this loop
+		# else continue the loop for the next door from the end of the path
+		if len(valid_points) > 0:
+			break
+		i += 1
+		valid_points = []
+	
+	# choose a valid placement for the door and give it target rooms
+	var door_point = valid_points[_rng.randi() % len(valid_points)]
+	var next_level = next_levels[_rng.randi() % len(next_levels)]
+	var next_room = next_rooms[_rng.randi() % len(next_rooms)]
+	var door = _doorway.instance()
+	door.next_level = next_level
+	door.next_room = next_room
+	door.global_position = door_point + _tile_size.project(Vector2.UP) / 2
+	add_child(door)
 
 
 func _get_spawn_multiplier(worldv: Vector2) -> float:
@@ -122,6 +184,9 @@ func _find_empty_worldvs() -> PoolVector2Array:
 		for tilev in room_tilevs:
 			var tile_offset = _tilev_to_worldv(tilev)
 			var worldv = room_offset + tile_offset + _tile_size/2
+			# check if this position is colliding with a tile (technically
+			# check if there is a collision on the world layer, which should
+			# just be tiles)
 			var result = space_state.intersect_point(worldv, 32, [], 1, true)
 			if result:
 				continue
