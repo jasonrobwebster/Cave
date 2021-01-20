0e88a09485cf94852784c45816235ef936e69573
jasonrobwebster <jasonrobwebster@gmail.com>
2021-01-20 21:26:03 +0200

    minor tweaks to player code

--- player/PlayerState.gd
+++ player/PlayerState.gd
@@ -3,6 +3,8 @@ extends State
 signal start_run()
 signal end_run()
 
+var velocity: Vector2 = Vector2.ZERO
+
 onready var sprite: Sprite = owner.get_node("Pivot/Sprite")
 onready var pivot: Position2D = owner.get_node("Pivot")
 onready var anim_player: AnimationPlayer = owner.get_node("AnimationPlayer")
@@ -23,10 +25,3 @@ func _get_input_x() -> float:
 func _get_input_run(run_multiplier: float = 1.0) -> float:
 	var input_run = Input.get_action_strength("run") * run_multiplier + 1.0
 	return input_run
-
-
-func handle_input(event: InputEvent):
-	if event.is_action_pressed("run"):
-		emit_signal("start_run")
-	if event.is_action_released("run"):
-		emit_signal("end_run")
